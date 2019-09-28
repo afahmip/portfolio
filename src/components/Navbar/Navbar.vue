f1b63f5d82635f108440024ec67b91f4a8044c8b
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-09-29 04:29:01 +0700

    Added homepage header

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <div class="nav">
+      <router-link to="/" class="hoverable">
+        ahmad<b>fahmi</b>
+      </router-link>
+      <div class="menu">
+        <router-link to="/">
+          profile
+        </router-link>
+        <router-link to="/">
+          resume
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  @include page;
+  padding: 1.5rem 0;
+  display: flex;
+  justify-content: space-between;
+  @include mobile {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+}
+
+a {
+  font-size: 1.1rem;
+  color: black;
+  font-family: 'Noticia Text', serif;
+  @include mobile {
+    font-size: 0.85rem;
+  }
+}
+
+.menu > a {
+  margin-left: 2rem;
+  @include mobile {
+    margin-left: 1.1rem;
+  }
+}
+</style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home page">
+    <div class="header">
+      <h1>frontend & ui/ux <span>enthusiast</span><br>self-claimed <i>hackathlete</i><br>based in Bandung, ID_</h1>
+      <p>Tech geek, but designer by heart.</p>
+      <p>
+        Former Software Engineer Intern at
+        <a href="https://www.blibli.com">Blibli</a> and
+        <a href="http://mapan.id">Mapan</a>.
+      </p>
+      <p>In love with web development.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss" scoped>
+.header > h1 {
+  font-family: 'Noticia Text', serif;
+  font-size: 2.25rem;
+  margin-bottom: 2rem;
+  span { color: $blue; }
+  @include mobile {
+    font-size: 4.5vh;
+  }
+}
+
+.header > p {
+  font-size: 1.1rem;
+  line-height: 1.8rem;
+  a {
+    font-weight: bold;
+    color: $blue;
+  }
+  @include mobile {
+    font-size: 2.25vh;
+    line-height: 3.5vh;
+  }
+}
+</style>
